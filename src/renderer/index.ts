b35e7942cdab0c1160033c26d9e3d7a916755b94
KomoriDev <mute231010@gmail.com>
2024-10-02 15:52:00 +0800

    :heavy_plus_sign: 使用 tailwindcss

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import { router } from './router';
 import App from './App.vue';
 
+import './index.css';
+
 export const onSettingWindowCreated = async (view: HTMLElement) => {
   const app = createApp(App);
   app.use(router);
