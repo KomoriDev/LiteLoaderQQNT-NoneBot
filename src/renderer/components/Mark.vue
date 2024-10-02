a43f98ffc4cda63d66eb53fd36091b53ede8232d
KomoriDev <mute231010@gmail.com>
2024-10-02 17:40:21 +0800

    :lipstick: 添加组件

---
+++ src/renderer/components/Mark.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="mark">
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped>
+.mark {
+  color: inherit;
+  background-color: transparent;
+
+  animation-name: mark-highlight-animation;
+  animation-duration: 1.5s;
+  animation-delay: 0.5s;
+  animation-fill-mode: forwards;
+
+  background-size: 200%;
+  background-position: 100% 0.4lh;
+  background-repeat: no-repeat;
+  background-image: linear-gradient(to right, #ffce0050 50%, transparent 50%);
+}
+
+@keyframes mark-highlight-animation {
+  from {
+    color: inherit;
+    background-position: 100% var(--vp-nolebase-mark-bg-shift);
+  }
+
+  to {
+    color: var(--vp-c-text-1);
+    background-position: 0% var(--vp-nolebase-mark-bg-shift);
+  }
+}
+</style>
